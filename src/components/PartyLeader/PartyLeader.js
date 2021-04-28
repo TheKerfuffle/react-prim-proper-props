6b23c026edb0cbb61fab2fe40cb05ddded7988a8
Charlie Stokes <chstokes7@gmail.com>
2021-04-28 16:54:04 -0500

    Worked through base mode pretty easily, working on stretch goals!

---
+++ src/components/PartyLeader/PartyLeader.js
@@ -0,0 +1,13 @@
+
+
+function PartyLeader(props) {
+    
+    return (
+        <>
+        <h2>Party Leader</h2>
+        <h3>{props.leader.name}</h3>
+        </>
+    )
+}
+
+export default PartyLeader;
